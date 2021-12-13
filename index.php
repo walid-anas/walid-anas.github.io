bebc2b9f567ae73849f40bc9ab47bbbf78134fbf
walid-anas <96084256+walid-anas@users.noreply.github.com>
2021-12-13 21:44:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html >
+<head>
+<title>Connexion à  la base</title>
+</head>
+<body>
+    
+   <table border="1" >
+    <h2>  Connexion à  la base </h2>
+    <tr > 
+       <td  width="130"><div align="left" >&nbsp;&nbsp;&nbsp;Code département</div></td>
+       <td  width="270"><div align="center"  >&nbsp;&nbsp;&nbsp;Département</div></td>
+       <td width="150"><div align="left" >&nbsp;Nom Responsable</div></td>
+       <td width="200"><div align="left" >&nbsp;Contact</div></td>
+	   <td width="200"><div align="left" >&nbsp;Nombre des étudiants</div></td>
+    </tr>					
+
+									  
+</Table>
+ </body>
+</html>
